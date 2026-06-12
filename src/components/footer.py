9392141e74155c561a4522d5616a50801129292c
amritanshu2005 <amritanshuchaudhary60@gmail.com>
2026-06-12 16:37:10 +0530

    Initial commit

---
+++ src/components/footer.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def footer_home():
+    st.markdown("<div style='text-align:center ;font-weight:bold ;color:white;'>Created with ❤️ by</div>",
+                unsafe_allow_html=True)
+    st.markdown("<div style='text-align:center;font-weight:bold;'>Amritanshu</div>",
+                unsafe_allow_html=True)
+    
+def footer_dashboard():
+    st.markdown("<div style='text-align:center ;font-weight:bold ;color:black;'>Created with ❤️ by</div>",
+                unsafe_allow_html=True)
+    st.markdown("<div style='text-align:center;font-weight:bold;'>Amritanshu</div>",
+                unsafe_allow_html=True)
+
